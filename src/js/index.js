2200da9a600abfc489731348e69dcb5ea0347234
nc <nelsonc19@gmail.com>
2017-11-21 11:55:48 -0500

    Add color toggle button in header

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 (function() {
   
+  // Only runs on home page
   if (document.getElementById('particles-js')) {
-    var url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=nnaiiil&api_key=4cc9b31d9dfb24d9ae40c9963e7aee9f&format=json';
+    const url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=nnaiiil&api_key=4cc9b31d9dfb24d9ae40c9963e7aee9f&format=json';
     fetch(url).then(function (response) {
       return response.json();
     }).then(function (data) {
@@ -17,15 +18,30 @@
     });
     
     particlesJS.load('particles-js', '../particlesjs-config.json', function() {
-      console.log('callback - particles.js config loaded');
+      // console.log('callback - particles.js config loaded');
+    });
+
+    document.querySelector('.toggle-section').addEventListener('click', function() {
+      toggleTheme();
     });
   };
 
-  var menuBtn = document.querySelector('.nav-menu-btn');
-  var menuContainer = document.querySelector('.menu');
+  const menuBtn = document.querySelector('.nav-menu-btn');
+  const menuContainer = document.querySelector('.menu');
   menuBtn.addEventListener('click', function() {
     menuBtn.classList.toggle('change');
     menuContainer.classList.toggle('open');			
   })
 
 })();
+
+function toggleTheme() {
+  const canvas = document.getElementById('particles-js');
+  const header = canvas.nextSibling.nextSibling;
+  header.classList.toggle('light-theme');
+  header.classList.toggle('dark-theme');
+  canvas.classList.toggle('light-theme');
+  canvas.classList.toggle('dark-theme');
+  document.querySelector('.toggle-btn').classList.toggle('light-theme');
+  document.querySelector('.toggle-switch').classList.toggle('light-theme');
+}
